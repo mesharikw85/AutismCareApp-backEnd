cbd1f96af630f0afc83500c23f8e8a94a1ac2fc3
mesharikw85 <mkwg85@gmail.com>
2023-08-04 23:31:01 +0300

    addallmodal

--- models/User.js
+++ models/User.js
@@ -1,21 +1,32 @@
 const { model, Schema } = require("mongoose");
 
-const userSchema = new Schema(
+const UserSchema = new Schema(
   {
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
     email: { type: String, required: true },
-    nationality: { type: String, required: true },
-    image: {
-      type: String,
-      default: "/",
+    languagen: { type: String, required: true },
+    image: { type: String, required: true },
+    addresses: [
+      {
+        government: { type: String, required: true },
+        city: { type: String, required: true },
+        block: { type: String, required: true },
+        street: { type: String, required: true },
+        avenue: { type: String, required: true },
+        house: { type: String, required: true },
+        street: { type: String, required: true },
+      },
+    ],
+    numbers: {
+      type: [Number],
+      required: true,
     },
-    // relations
-    phone: { type: Schema.Types.ObjectId, ref: "PhoneNumber" },
+    child: { type: Schema.Types.ObjectId, ref: "ProfileChild" },
   },
   { timestamps: true }
 );
 
-module.exports = model("User", userSchema);
+module.exports = model("User", UserSchema);
