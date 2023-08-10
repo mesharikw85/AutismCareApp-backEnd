149d1ecf1e01fdd22b020a660380d7112a25e8df
mesharikw85 <mkwg85@gmail.com>
2023-08-10 17:59:02 +0300

    doneaddfun

--- api/user/User.controllers.js
+++ api/user/User.controllers.js
@@ -1,8 +1,8 @@
 const User = require("../../models/User");
+const ChildProfile = require("../../models/ProfileChild");
 const generateToken = require("../../utils/auth/generateToken");
 const generateTpken = require("../../utils/auth/generateToken");
 const passhash = require("../../utils/auth/passhash");
-const hashPaswoord = require("../../utils/auth/passhash");
 
 exports.fetchUser = async (userId, next) => {
   try {
@@ -72,5 +72,65 @@ exports.updateprofile = async (req, res, next) => {
     return res.status(204).end();
   } catch (error) {
     return next({ status: 400, message: error.message });
+  }
+};
+
+//profilechild
+exports.createProfileChild = async (req, res, next) => {
+  try {
+    if (req.file) {
+      req.body.image = `${req.file.path.replace("\\", "/")}`;
+    }
+    if (!req.body.image)
+      return next({ status: 400, message: "no image was uploaded!" });
+    const newChild = await ChildProfile.create(req.body);
+    res.status(201).json(newChild);
+  } catch (error) {
+    return next({ status: 400, message: error.message });
+  }
+};
+
+//getProfileChild
+exports.getProfileChild = async (req, res, next) => {
+  try {
+    // Fetch child profiles using ChildProfile model
+    const childProfiles = await ChildProfile.find().select(
+      "-__v -password -email"
+    );
+
+    // Sending the fetched child profiles as a JSON response
+    return res.status(200).json(childProfiles);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateprofileChild = async (req, res, next) => {
+  try {
+    const updatedData = req.body;
+
+    if (req.file) {
+      updatedData.image = req.file.path.replace("\\", "/");
+    }
+
+    // Find the child profile by ID and update it
+    const updatedChild = await ChildProfile.findByIdAndUpdate(
+      req.user._id, // Assuming req.user._id contains the correct user ID
+      updatedData,
+      {
+        new: true, // Return the updated document
+      }
+    );
+
+    if (!updatedChild) {
+      // If updatedChild is null, the profile was not found
+      return res.status(404).json({ error: "Child profile not found." });
+    }
+
+    console.log("Updated Child:", updatedChild);
+    return res.status(200).json(updatedChild);
+  } catch (error) {
+    console.error("Error updating child profile:", error);
+    return res.status(400).json({ error: "Failed to update child profile." });
   }
 };
--- models/ProfileChild.js
+++ models/ProfileChild.js
@@ -2,28 +2,13 @@ const { model, Schema } = require("mongoose");
 
 const ProfileChildSchema = new Schema(
   {
-    firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
-    birthday: { type: String, required: true },
-    doctorname: { type: String, required: true },
-    image: { type: String, required: true },
-    addresses: [
-      {
-        government: { type: String, required: true },
-        city: { type: String, required: true },
-        block: { type: String, required: true },
-        street: { type: String, required: true },
-        avenue: { type: String, required: true },
-        house: { type: String, required: true },
-        street: { type: String, required: true },
-      },
-    ],
-
-    phones: [
-      {
-        number: "string",
-      },
-    ],
+    firstname: { type: String, required: false },
+    lastname: { type: String, required: false },
+    birthday: { type: String, required: false },
+    doctorname: { type: String, required: false },
+    image: { type: String, required: false },
+    phones: String,
+    addresses: String,
     user: { type: Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
